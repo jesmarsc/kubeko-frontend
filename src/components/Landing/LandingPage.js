db0eaee3720a6fc1c0233e2f9575556f75bb97b1
Jesmar Castillo <jesmar@ucsb.edu>
2019-12-02 19:11:41 -0800

    Add features to landing page

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import Lottie from 'react-lottie';
 import { Button } from 'antd';
 
+import workloadDash from '@assets/workload-dash-cropped.png';
+import clusterDash from '@assets/cluster-dash-cropped.png';
 import Navbar from './Navbar/LandingNav';
 import classes from './LandingPage.module.scss';
 import animation from '@assets/banner.json';
@@ -22,7 +24,7 @@ const Landing = () => {
       <Navbar />
 
       <div className={classes.container}>
-        <header className={classes.header}>
+        <header className={classes.banner}>
           <div className={classes.wrapper}>
             <h1 className={classes.title}>Kubernetes Konekt</h1>
             <h2 className={classes.subtitle}>Cloud. Made simple.</h2>
@@ -43,12 +45,39 @@ const Landing = () => {
           <Lottie
             isClickToPauseDisabled={true}
             style={{
-              height: '100%',
+              maxHeight: '100%',
               gridArea: 'b',
             }}
             options={defaultOptions}
           />
         </header>
+        <section className={classes.section}>
+          <h1 className={classes.section__title}>Why Kubeko?</h1>
+          <div className={classes.features}>
+            <div className={classes.featureCard}>
+              <img
+                className={classes.featureCard__image}
+                src={workloadDash}
+                alt="alt text"
+              />
+              <h1 className={classes.featureCard__feature}>Create Workloads</h1>
+              <h2 className={classes.featureCard__description}>
+                As simple as uploading a YAML file!
+              </h2>
+            </div>
+            <div className={classes.featureCard}>
+              <img
+                className={classes.featureCard__image}
+                src={clusterDash}
+                alt="alt text"
+              />
+              <h1 className={classes.featureCard__feature}>Manage Users</h1>
+              <h2 className={classes.featureCard__description}>
+                See who is using your cluster in real-time!
+              </h2>
+            </div>
+          </div>
+        </section>
       </div>
     </Fragment>
   );
